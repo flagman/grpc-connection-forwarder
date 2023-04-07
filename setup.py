eef9a13d9ba733026463501091cc1da7d2264865
Pavel Malai <flagmansupport@gmail.com>
2023-04-07 11:33:47 +0200

    [update] bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="grpc_connection_forwarder",
-    version="0.1.4",
+    version="0.1.5",
     author="Pavel Malay",
     author_email="flagmansupport@gmail.com",
     description="A small package for counting gRPC connections",
